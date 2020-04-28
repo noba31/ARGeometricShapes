4f7651fc4c16b59fbaf2e5506fcdbc5ba40f12a7
Noumika Balaji <noumika.balaji@infovision.net>
2020-04-27 19:10:13 -0500

    Adding source code

---
+++ RuntimeModels/app/src/main/java/com/example/runtimemodels/MainActivity.java
@@ -0,0 +1,86 @@
+package com.example.runtimemodels;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.Button;
+
+import com.google.ar.core.Anchor;
+import com.google.ar.sceneform.AnchorNode;
+import com.google.ar.sceneform.math.Vector3;
+import com.google.ar.sceneform.rendering.Color;
+import com.google.ar.sceneform.rendering.MaterialFactory;
+import com.google.ar.sceneform.rendering.ModelRenderable;
+import com.google.ar.sceneform.rendering.ShapeFactory;
+import com.google.ar.sceneform.ux.ArFragment;
+
+public class MainActivity extends AppCompatActivity {
+
+    private ArFragment arFragment;
+
+    private enum ShapeType {
+        CUBE,
+        SPHERE,
+        CYLINDER
+    }
+
+    private ShapeType shapeType = ShapeType.CUBE;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        arFragment = (ArFragment) getSupportFragmentManager().findFragmentById(R.id.fragment);
+
+        Button cube = findViewById(R.id.cube);
+        Button sphere = findViewById(R.id.sphere);
+        Button cylinder = findViewById(R.id.cylinder);
+
+        arFragment.setOnTapArPlaneListener((hitResult, plane, motionEvent) -> {
+
+            if(shapeType == ShapeType.CUBE)
+                placeCube(hitResult.createAnchor());
+            else if(shapeType == ShapeType.SPHERE)
+                placeSphere(hitResult.createAnchor());
+            else
+                placeCylinder(hitResult.createAnchor());
+        });
+
+        cube.setOnClickListener(view -> shapeType = ShapeType.CUBE);
+        sphere.setOnClickListener(view -> shapeType = ShapeType.SPHERE);
+        cylinder.setOnClickListener(view -> shapeType = ShapeType.CYLINDER);
+    }
+
+    private void placeCylinder(Anchor anchor) {
+        MaterialFactory.makeOpaqueWithColor(this, new Color(android.graphics.Color.BLUE))
+                .thenAccept(material -> {
+                    ModelRenderable modelRenderable = ShapeFactory.makeCylinder(0.1f, 0.2f, new Vector3(0f, 0.2f, 0f), material);
+                    placeModel(modelRenderable, anchor);
+                });
+    }
+
+    private void placeSphere(Anchor anchor) {
+        MaterialFactory.makeOpaqueWithColor(this, new Color(android.graphics.Color.BLUE))
+                .thenAccept(material -> {
+                    ModelRenderable modelRenderable = ShapeFactory.makeSphere(0.1f, new Vector3(0f, 0.1f, 0f), material);
+                    placeModel(modelRenderable, anchor);
+                });
+    }
+
+    private void placeCube(Anchor anchor) {
+        MaterialFactory
+                .makeOpaqueWithColor(this, new Color(android.graphics.Color.BLUE))
+                .thenAccept(material -> {
+                    ModelRenderable modelRenderable = ShapeFactory.makeCube(new Vector3(0.1f, 0.1f, 0.1f), new Vector3(0f, 0.1f, 0f), material);
+
+                    placeModel(modelRenderable, anchor);
+                });
+    }
+
+    private void placeModel(ModelRenderable modelRenderable, Anchor anchor) {
+        AnchorNode anchorNode = new AnchorNode(anchor);
+        anchorNode.setRenderable(modelRenderable);
+        arFragment.getArSceneView().getScene().addChild(anchorNode);
+    }
+}
